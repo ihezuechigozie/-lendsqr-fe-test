6474d1fc598bac4c267fbee9698ef75a88ec64d2
ihezuechigozie <ihezuechigozie@gmail.com>
2025-01-31 17:32:13 +0100

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,7 @@ import Dashboard from './Components/pages/Dashboard'
 import { Route, BrowserRouter , Routes,} from 'react-router-dom'
 import Display from './Display'
 import UsersFilter from './Components/pages/Usersfilter'
-import UserCard from './Components/Dashboard/UserCard'
 import Userdetails from './Components/pages/Userdetails'
-import DetailsTable from './Components/Dashboard/DetailsTable'
-import UserTable from './Components/Dashboard/UserTable'
-import Sidebardrop from './Components/Dashboard/Sidebardrop'
-import Topbar from './Components/Dashboard/TopBar'
-import Sidebar from './Components/Dashboard/SideBar'
-import Smallsreen from './Components/Dashboard/Smallscreen1'
-import UserTable3 from './Components/Dashboard/Usertable3'
-import Filterdrop from './Components/Dashboard/Filterdrop'
-
 
 
 function App() {
@@ -29,16 +19,7 @@ function App() {
         <Route path='/' element={<Display/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/Usersfilter' element={<UsersFilter/>}/>
-        <Route path='/UserCard' element={<UserCard/>}/>
         <Route path='/Userdetails' element={<Userdetails/>}/>
-        <Route path='/DetailsTable' element={<DetailsTable/>}/>
-        <Route path='/UserTable' element={<UserTable/>}/>
-        <Route path='/Sidebardrop' element={<Sidebardrop/>}/>
-        <Route path='/TopBar' element={<Topbar/>}/>
-        <Route path='/SideBar' element={<Sidebar/>}/>
-        <Route path='/Smallscreen1' element={<Smallsreen/>}/>
-        <Route path='/Usertable3' element={<UserTable3/>}/>
-        <Route path='/Filterdrop' element={<Filterdrop/>}/>
       </Routes>
     </BrowserRouter> 
 
--- src/Display.tsx
+++ src/Display.tsx
@@ -14,38 +14,10 @@ const Display: React.FC =()=>{
       <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Usersfilter">
         <button className="login-btn">Users Filter</button>
       </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/UserCard">
-        <button className="login-btn">UserCard</button>
-      </Link>
       <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Userdetails">
         <button className="login-btn">Userdetails</button>
       </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/DetailsTable">
-        <button className="login-btn">DetailsTable</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/UserTable">
-        <button className="login-btn">UserTable</button>
-      </Link>
-      <br />
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Sidebardrop">
-        <button className="login-btn">Sidebar DropDown</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/TopBar">
-        <button className="login-btn">Top Bar</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/SideBar">
-        <button className="login-btn">Side Bar</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Smallscreen1">
-        <button className="login-btn">Small Screen</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Usertable3">
-        <button className="login-btn">User Table 3</button>
-      </Link>
-      <Link style={{ textDecoration: "none", marginLeft: "40px" }} to="/Filterdrop">
-        <button className="login-btn">Filter drop</button>
-      </Link>
-
+     
     </>
   )
 
